29180db11f0081ba39af2bc17f242972f05e16f2
Chad Braden <quesog@Chads-Mac.local>
2013-08-24 22:09:19 -0400

    eh?

---
+++ File.cpp
@@ -0,0 +1,9 @@
+//
+//  File.cpp
+//  
+//
+//  Created by Chad Braden on 8/24/13.
+//
+//
+
+#include "File.h"
---
+++ File.h
@@ -0,0 +1,14 @@
+//
+//  File.h
+//  
+//
+//  Created by Chad Braden on 8/24/13.
+//
+//
+
+#ifndef ____File__
+#define ____File__
+
+#include <iostream>
+
+#endif /* defined(____File__) */
